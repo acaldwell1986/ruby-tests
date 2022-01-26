e0640e1c5573fd11cf1cda02f17ee85d53af9e9b
acaldwell1986 <acaldwell1986@gmail.com>
2022-01-26 14:45:55 -0500

    Finished caesar cipher function

--- caesar_cipher/caeser_cipher.rb
+++ caesar_cipher/caeser_cipher.rb
@@ -1,10 +1,46 @@
-require 'pry-byebug'
+def caesar_cipher(word, shift)
+  word_array = word.chars
+  return_array = []
 
-def hello_world
-  puts "Hello world!"
-end
+  while shift > 26 do
+    shift -= 26
+  end
 
-def caesar_cipher(string, num)
-  
+  p word_array
+  word_array.each_with_index do |t, idx|
+    char_val = word_array[idx].ord 
+    value = word_array[idx].ord + shift                         
+    
+    if char_val.between?(65,90)
+
+      if value <= 90
+        return_array << char_val + shift
+      elsif value > 90 && char_val <= 90
+        temp = 91 - char_val
+        temp2 = shift - temp 
+        char_val = 65 + temp2
+        return_array << char_val
+      end
+
+    elsif char_val.between?(97,122)
+      if value <= 122
+        return_array << char_val + shift
+      elsif value > 122 && char_val <= 122
+        temp = 123 - char_val
+        temp2 = shift - temp 
+        char_val = 97 + temp2
+
+        return_array << char_val
+      end
+    else
+      return_array << char_val
+    end
+  end
+
+    new_array = []
+    return_array.each do |element|
+      new_array << element.chr
+    end
+    return new_array.join("")
 
 end
