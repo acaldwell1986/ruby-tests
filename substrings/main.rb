a2ecca7bd638e101217d50eb6a4d3c98754b3dbf
acaldwell1986 <acaldwell1986@gmail.com>
2022-01-27 16:31:27 -0500

    Finished substring function

--- substrings/main.rb
+++ substrings/main.rb
@@ -1,26 +1,41 @@
+# Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
+
+#Example dictionary and word values
 example_dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+word = "Howdy partner, sit down! How's it going?"
 
-word = "below"
-word_save = word[0..word.length]
-word_array = []
+#function that returns the hash
+def substrings(word, dictionary)
+  word = word.downcase
+  word_array = []
+  match_array = []
 
-# takes word and saves each possible combination of words into word_array for comparison
-# mutates the original word, so the word_save variable is used to retain the value in case it is needed
-i = word.length
-until i <= 0 do
-  word_array << word[0..i-1]
-  i -= 1
+  #creates a word_array with every possible combination of words from the given word argument
+  i = word.length
+  until i <= 0 do
+    word_array << word[0..i-1]
+    i -= 1
+    if word.length > 0 && i == 0
+      word[0] = ''
+      i = word.length
+    end
+  end
 
-  if word.length > 0 && i == 0
-    word[0] = ''
-    i = word.length
+  #performs a comparison between the dictionary and the word_array and stores any matches in match_array
+  dictionary.each do |element|
+    word_array.each do |fragment|
+      if element == fragment
+        match_array << fragment
+      end
+    end
+  end
+
+  #reduce function returns the matched fragments and the count of each
+  word_hash = match_array.reduce(Hash.new(0)) do |fragment, count|
+    fragment[count] += 1
+    fragment
   end
 end
 
-#write function to compare both arrays, if true return count of words in the dictionary
-
-
-
-
-puts word_array
-puts word_save
+#example function call
+puts substrings(word, example_dictionary)
