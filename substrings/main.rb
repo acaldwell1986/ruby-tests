fd9cbcc05bce2d2360812f7a9c03a22e873b3d60
acaldwell1986 <acaldwell1986@gmail.com>
2022-01-28 14:21:11 -0500

    Added comments

--- substrings/main.rb
+++ substrings/main.rb
@@ -1,4 +1,8 @@
-# Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
+# Implement a method #substrings that takes a word as the
+# first argument and then an array of valid substrings 
+# (your dictionary) as the second argument. It should return a hash
+# listing each substring (case insensitive) that was found in the original string
+# and how many times it was found.
 
 #Example dictionary and word values
 example_dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
