be0d9e1403788e701cf4c006e1980aefb055180c
Andrew C <acaldwell1986@gmail.com>
2022-02-08 19:02:13 -0500

    formatting

--- stockpicker/stockpicker.rb
+++ stockpicker/stockpicker.rb
@@ -1,17 +1,16 @@
 stocks = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(prices)
-hash_positions = []
+  hash_positions = []
 
   prices.each_with_index do |element, i|
-    
     k = 0
     for k in i+1...prices.length
       temp = prices[i] - prices[k]
       hash_positions.push({result: temp, position: [i,k]})
     end
   end
-  
+
 return hash_positions.min_by{|k| k[:result]}[:position]
 end
 
